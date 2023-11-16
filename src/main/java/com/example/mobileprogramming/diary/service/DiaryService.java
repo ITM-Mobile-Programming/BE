4417f3fd78f63209c3c0c1a870c04c2f1012c4d0
반영환 <lopahn2@gmail.com>
2023-11-16 18:00:06 +0900

    t

--- src/main/java/com/example/mobileprogramming/diary/service/DiaryService.java
+++ src/main/java/com/example/mobileprogramming/diary/service/DiaryService.java
@@ -1,5 +1,8 @@
 package com.example.mobileprogramming.diary.service;
 
+import com.example.mobileprogramming.diary.dto.ReqWriteDiaryDto;
+
 public interface DiaryService {
+    void saveDiary(ReqWriteDiaryDto reqWriteDiaryDto);
 
 }
--- src/main/java/com/example/mobileprogramming/diary/service/DiaryServiceImpl.java
+++ src/main/java/com/example/mobileprogramming/diary/service/DiaryServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.mobileprogramming.diary.service;
 
+import com.example.mobileprogramming.diary.dto.ReqWriteDiaryDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DiaryServiceImpl implements DiaryService{
+    @Override
+    public void saveDiary(ReqWriteDiaryDto reqWriteDiaryDto) {
 
+    }
 }
